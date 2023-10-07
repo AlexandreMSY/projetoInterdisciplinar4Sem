f1164d7c47b8d6bdf2c9dcdc0c2ad08af0dd0d7f
Alexandre Massao <alexandremsubs@gmail.com>
2023-10-07 16:25:52 -0300

    feito serviço para cancelar consultas

---
+++ server/services/consulta/cancelarConsulta.js
@@ -0,0 +1,23 @@
+const Consultas = require("../../db/models/Consultas");
+const procurarUsuario = require("../usuario/procurarUsuario");
+
+const cancelarConsulta = async (autenticacao, consultaId) => {
+  const { senha, email } = autenticacao;
+  const usuarioEncontrado = await procurarUsuario(email, senha);
+
+  if (usuarioEncontrado) {
+    const colunasApagadas = await Consultas.destroy({
+      where: {
+        consulta_id: consultaId,
+      },
+    });
+
+    return colunasApagadas === 1
+      ? { colunasApagadas: true }
+      : { colunasApagadas: false };
+  } else {
+    return { mensagem: "Usuário não encontrado" }
+  }
+};
+
+module.exports = cancelarConsulta
