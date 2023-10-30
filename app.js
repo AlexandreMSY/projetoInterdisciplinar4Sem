c2f77f34758038c390c390404746953b62116c19
Alexandre Massao <alexandremsubs@gmail.com>
2023-10-30 14:16:43 -0300

    adicionado porta do servidor no .env

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const usuarioRouter = require("./routes/usuario/usuarioRoutes");
-const consultaRouter = require("./routes/consulta/consultaRoutes")
+const consultaRouter = require("./routes/consulta/consultaRoutes");
 const { sync } = require("./db/dbConexao");
 let cors = require("cors");
+require("dotenv").config();
 
 /*
   https://stackoverflow.com/questions/70148975/why-is-req-body-undefined
@@ -15,8 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/usuario", usuarioRouter);
-app.use("/api/consulta", consultaRouter)
+app.use("/api/consulta", consultaRouter);
 
-app.listen(8000, () => {
-  console.log("Porta 8000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", () => {
+  console.log("Porta " + process.env.SERVER_PORT);
 });
